2c1e2d756da16a58b46c7d31f17e40490b829fca
IvanManso <ivan.mg313@gmail.com>
2022-01-18 14:29:15 +0100

    comentario añadido a main

--- Entornos/src/Main.java
+++ Entornos/src/Main.java
@@ -7,13 +7,13 @@ import java.io.InputStreamReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 /**
- * Clase que prueba los métodos de la clase CCuenta
- * @author IvánManso
+ * Clase que prueba los mï¿½todos de la clase CCuenta
+ * @author Ivï¿½nManso
  * @version v1.0
  * @since 30/11/2021
  */
 public class Main {
-	static BufferedReader dato = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader dato = new BufferedReader(new InputStreamReader(System.in));//comentario para el git
 /**
  * 
  * @param args
@@ -22,41 +22,41 @@ public class Main {
 		InterfazCuentaCorriente cuenta1;
 		double saldoActual;
 		int opcion = 0;
-		cuenta1 = new CuentaCorriente("Juan López", "1000-2365-85-123456789", 2500, 0);
+		cuenta1 = new CuentaCorriente("Juan Lï¿½pez", "1000-2365-85-123456789", 2500, 0);
 		do {
 			try {
-				System.out.println("MENÚ DE OPERACIONES");
+				System.out.println("MENï¿½ DE OPERACIONES");
 				System.out.println("-------------------");
 				System.out.println("1 - Ingresar");
 				System.out.println("2 - Retirar");
 				System.out.println("3 - Finalizar");
 				opcion = Integer.parseInt(dato.readLine());
 				if (opcion == 1) {
-					System.out.println("¿Cuánto desea ingresar?: ");
+					System.out.println("ï¿½Cuï¿½nto desea ingresar?: ");
 					float ingresar = Integer.parseInt(dato.readLine());
 					operativa_cuenta(cuenta1, ingresar, opcion);
 				} else if (opcion == 2) {
-					System.out.println("¿Cuátno desea retirar?: ");
+					System.out.println("ï¿½Cuï¿½tno desea retirar?: ");
 					float retirar = Integer.parseInt(dato.readLine());
 					operativa_cuenta(cuenta1, retirar, opcion);
 				} else if (opcion == 3) {
-					System.out.println("Finalizamos la ejecución");
+					System.out.println("Finalizamos la ejecuciï¿½n");
 				} else {
-					System.err.println("Opción errónea");
+					System.err.println("Opciï¿½n errï¿½nea");
 				}
 /*               switch(opcion){
                 case 1:
-                    System.out.println("¿Cuánto desea ingresar?: ");
+                    System.out.println("ï¿½Cuï¿½nto desea ingresar?: ");
                     float ingresar=Integer.parseInt(dato.readLine());
                     operativa_cuenta(cuenta1,ingresar,opcion);
                     break;
                 case 2:
-                    System.out.println("¿Cuátno desea retirar?: ");
+                    System.out.println("ï¿½Cuï¿½tno desea retirar?: ");
                     float retirar=Integer.parseInt(dato.readLine());
                     operativa_cuenta(cuenta1,retirar,opcion);
                     break;
                 case 3:
-                    System.out.println("Finalizamos la ejecución");
+                    System.out.println("Finalizamos la ejecuciï¿½n");
             }
 */
 			} catch (IOException ex) {
@@ -65,8 +65,8 @@ public class Main {
 		} while (opcion != 3);
 		saldoActual = cuenta1.getSaldo();
 		System.out.println("El saldo actual es: " + saldoActual);
-		InterfazCuentaCorriente cuenta2 = new CuentaCorriente("Jaime López", "1444-2445-84-123456789", 1000, 0);
-		visualizarDatosCuenta(cuenta1, cuenta2, "Buenos días");
+		InterfazCuentaCorriente cuenta2 = new CuentaCorriente("Jaime Lï¿½pez", "1444-2445-84-123456789", 1000, 0);
+		visualizarDatosCuenta(cuenta1, cuenta2, "Buenos dï¿½as");
 	}
 /**
  * @param cuenta1
@@ -79,7 +79,7 @@ private static void visualizarDatosCuenta(InterfazCuentaCorriente cuenta1, Inter
 	System.out.println(cuenta2.toString());
 }
 /**
- * Método para retirar o ingresar dinero en una cuenta
+ * Mï¿½todo para retirar o ingresar dinero en una cuenta
  * @param cuenta1
  * @param cantidad
  * @param opcion
